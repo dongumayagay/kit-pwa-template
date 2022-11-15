32e749bd228c225e5637963089b775ad6afea86c
Don Gumayagay <dngumayagay@gmail.com>
2022-11-16 06:09:07 +0800

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,11 @@
-<script>import "../app.postcss";</script><slot></slot>
+<script>
+	import '../app.postcss';
+	import AppBar from './AppBar.svelte';
+	import BottomNav from './BottomNav.svelte';
+</script>
+
+<main class="bg-base-200 min-h-full flex flex-col items-center">
+	<AppBar />
+	<slot />
+	<BottomNav />
+</main>
---
+++ src/routes/AppBar.svelte
@@ -0,0 +1,3 @@
+<div class="navbar bg-base-100 sticky top-0">
+	<h1 class="normal-case text-xl text-primary font-black">Test</h1>
+</div>
